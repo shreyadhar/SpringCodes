45d04d9f21e7efce41fc5ef32957f1744644fbe9
shreyadhar <51167259+shreyadhar@users.noreply.github.com>
2019-06-14 10:33:47 +0530

    Update Test.java

--- FirstSpringProject/src/FirstExample/Test.java
+++ FirstSpringProject/src/FirstExample/Test.java
@@ -14,3 +14,24 @@ public static void main(String[] args) {
     student.displayInfo();  
 }  
 }  
+
+
+
+
+/**
+
+log4j:WARN No appenders could be found for logger (org.springframework.beans.factory.xml.XmlBeanDefinitionReader).
+log4j:WARN Please initialize the log4j system properly.
+Exception in thread "main" org.springframework.beans.factory.BeanDefinitionStoreException: IOException parsing XML document from class path resource [applicationContext.xml]; nested exception is java.io.FileNotFoundException: class path resource [applicationContext.xml] cannot be opened because it does not exist
+	at org.springframework.beans.factory.xml.XmlBeanDefinitionReader.loadBeanDefinitions(XmlBeanDefinitionReader.java:341)
+	at org.springframework.beans.factory.xml.XmlBeanDefinitionReader.loadBeanDefinitions(XmlBeanDefinitionReader.java:302)
+	at org.springframework.beans.factory.xml.XmlBeanFactory.<init>(XmlBeanFactory.java:73)
+	at org.springframework.beans.factory.xml.XmlBeanFactory.<init>(XmlBeanFactory.java:61)
+	at FirstExample.Test.main(Test.java:11)
+Caused by: java.io.FileNotFoundException: class path resource [applicationContext.xml] cannot be opened because it does not exist
+	at org.springframework.core.io.ClassPathResource.getInputStream(ClassPathResource.java:141)
+	at org.springframework.beans.factory.xml.XmlBeanDefinitionReader.loadBeanDefinitions(XmlBeanDefinitionReader.java:328)
+	... 4 more
+
+
+*/
